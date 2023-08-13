354aad459571227b81f6ebcb1d0605526a6addbd
Deveesh Shetty <deveeshshetty@gmail.com>
2023-08-13 10:36:14 +0530

    Adds UI to Image Box

--- src/components/image-box.tsx
+++ src/components/image-box.tsx
@@ -1,15 +1,21 @@
 // Component for dragging and dropping images which will be sent to ML Model for detection
+"use client"
+import LeafSVG from "@/components/assets/Leaf"
 
 export function ImageBox() {
+  // TODO: Add Drag and Drop Functionality
+
   return (
-    <section>
+    <section className="mt-8 md:mt-0">
       <form encType="multipart/form-data" method="post">
         <div className="flex flex-col items-center">
-          <label htmlFor="plant-image">
-            Choose the Plant Image to be detected
-            <div className="w-72 mt-4 flex items-center justify-center aspect-square mx-auto border-2 dark:border-white border-black border-dashed ">
-              <p>Drag and Drop the Image Here</p>
-
+          <label htmlFor="plant-image" className="cursor-pointer">
+            <p className="text-center">Click to Choose the Plant Image</p>
+            <div className="w-72 mt-4 flex items-center justify-center aspect-square mx-auto border-2 dark:border-white border-black border-dashed rounded-lg">
+              <div className="flex flex-col gap-2 p-4 justify-center items-center">
+                <LeafSVG />
+                <p className="text-center">Upload Leaf Image Here</p>
+              </div>
               <input
                 type="file"
                 name="plant-image"
