ab67a4b2aae8e69281ee806e636666676a5f91f3
Deveesh Shetty <deveeshshetty@gmail.com>
2023-08-15 11:46:15 +0530

    Improves responsive design and adds logo in mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ interface HeaderProps {
 
 export default function Header({ heading }: HeaderProps) {
   return (
-    <p className="scroll-m-20 text-center text-3xl md:text-5xl font-semibold tracking-tight transition-colors first:mt-0 gradient-text">
+    <p className="scroll-m-20 px-4 text-center text-3xl md:text-5xl font-semibold tracking-tight transition-colors first:mt-0 gradient-text">
       {heading}
     </p>
   )
--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -2,12 +2,20 @@ import { Button } from "@/components/ui/button"
 import Image from "next/image"
 import Link from "next/link"
 import GreenLeaves from "@/assets/green-leaves.jpg"
+import Logo from "@/assets/logo.png"
 
 export default function Introduction() {
   return (
     <section className="py-4 md:py-0 min-h-screen grid md:grid-cols-2 place-items-center">
-      <div className="w-11/12 md:w-1/2 text-center">
-        <h1 className="scroll-m-20 mb-4 text-3xl md:text-4xl font-extrabold tracking-tight lg:text-5xl uppercase gradient-text">
+      <div className="w-11/12 md:w-1/2 flex flex-col items-center text-center">
+        {/* TODO: Replace Logo Later */}
+        <Image
+          src={Logo}
+          alt="Rudraksh Logo"
+          width={150}
+          className="md:hidden"
+        />
+        <h1 className="scroll-m-20 mb-4 text-4xl font-extrabold tracking-tight lg:text-5xl uppercase gradient-text">
           Rudraksh
         </h1>
         <p className="text-center text-sm md:text-base mt-6">
