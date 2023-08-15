721e84d1a78b3abb5e0511e665b4bdf1bc0c9a06
Deveesh Shetty <deveeshshetty@gmail.com>
2023-08-15 21:46:49 +0530

    chore: minor changes and title change

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,10 +5,12 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center px-5 py-24 md:p-24">
       <div className="text-center mb-auto">
         <h1 className="scroll-m-20 mb-4 text-3xl md:text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Rudraksh - Plant Disease Detection
+          <span className="gradient-text">Rudraksh</span> - Plant Disease
+          Detection
         </h1>
         <h2 className="scroll-m-20 border-b pb-2 text-xl md:text-2xl font-semibold tracking-tight transition-colors first:mt-0">
-          Helping Mother Nature with Machine Learning
+          Helping <span className="gradient-text">Mother Nature</span> with
+          Machine Learning
         </h2>
       </div>
       <div className="mb-auto">
