23440caab03c2cc03a48547ca042e7c583b51cb6
Deveesh Shetty <deveeshshetty@gmail.com>
2023-08-15 10:01:47 +0530

    Adds Intro for Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import { ImageBox } from "@/components/image-box"
+import Introduction from "@/components/intro"
 
-export default function Home() {
+export default function Landing() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center px-5 py-24 md:p-24">
-      <div className="text-center mb-auto">
-        <h1 className="scroll-m-20 mb-4 text-3xl md:text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Rudraksh - Plant Disease Detection
-        </h1>
-        <h2 className="scroll-m-20 border-b pb-2 text-xl md:text-2xl font-semibold tracking-tight transition-colors first:mt-0">
-          Helping Mother Nature with Machine Learning
-        </h2>
-      </div>
-      <div className="mb-auto">
-        <ImageBox />
-      </div>
+    <main>
+      <Introduction />
     </main>
   )
 }
---
+++ src/components/intro.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import Link from "next/link"
+import GreenLeaves from "@/assets/green-leaves.jpg"
+
+export default function Introduction() {
+  return (
+    <section className="py-4 md:py-0 min-h-screen grid md:grid-cols-2 place-items-center">
+      <div className="w-11/12 md:w-1/2 text-center">
+        <h1 className="scroll-m-20 mb-4 text-3xl md:text-4xl font-extrabold tracking-tight lg:text-5xl uppercase gradient-text">
+          Rudraksh
+        </h1>
+        <p className="text-center text-sm md:text-base mt-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+          commodi itaque ea temporibus cum reprehenderit aliquid laudantium a in
+          odio eveniet recusandae officia culpa quo ducimus necessitatibus id
+          eos pariatur?
+        </p>
+        <Button className="my-7" asChild>
+          <Link href="/home">Use Rudraksh</Link>
+        </Button>
+      </div>
+      <div className="relative hidden md:block md:w-full md:h-full">
+        <Image
+          src={GreenLeaves}
+          alt="Green and Healthy Leaves"
+          fill
+          priority
+          quality={100}
+          className="block"
+        />
+      </div>
+    </section>
+  )
+}
