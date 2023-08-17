d661422a07b31b78a3b0e177b38afe39f41a24f3
Sannidhi Kaje <sannidhikaje04@gmail.com>
2023-08-17 22:55:25 +0530

    change ' to &apos;

--- src/components/faqs.tsx
+++ src/components/faqs.tsx
@@ -19,8 +19,8 @@ export default function FAQs() {
             <AccordionContent>Rudraksh is an AI-driven application designed to enhance plant well-being. It uses advanced machine learning algorithms to analyze images of plant leaves, detect diseases, and provide remedies for healthier plants. </AccordionContent>
           </AccordionItem>
           <AccordionItem value="question-2">
-            <AccordionTrigger>How accurate is Rudraksh's disease detection?</AccordionTrigger>
-            <AccordionContent>Rudraksh's accuracy is backed by a robust CNN model trained on a dataset of 87,000 images. While no model is perfect, our continuous learning and improvement processes ensure high accuracy levels. </AccordionContent>
+            <AccordionTrigger>How accurate is Rudraksh&apos;s disease detection?</AccordionTrigger>
+            <AccordionContent>Rudraksh&apos;s accuracy is backed by a robust CNN model trained on a dataset of 87,000 images. While no model is perfect, our continuous learning and improvement processes ensure high accuracy levels. </AccordionContent>
           </AccordionItem>
           <AccordionItem value="question-3">
             <AccordionTrigger>What benefits does Rudraksh offer over traditional plant care methods?</AccordionTrigger>
@@ -39,20 +39,20 @@ export default function FAQs() {
             <AccordionContent>Yes, Rudraksh not only identifies diseases but also offers insights and suggestions to prevent future occurrences. This proactive approach helps maintain healthier plants in the long run. </AccordionContent>
           </AccordionItem>
           <AccordionItem value="question-7">
-            <AccordionTrigger>Is Rudraksh's AI capable of recognizing multiple diseases?</AccordionTrigger>
-            <AccordionContent>Absolutely. Our AI model is trained on a diverse dataset, enabling it to recognize and diagnose a variety of plant diseases, ensuring comprehensive support for your plants' well-being. </AccordionContent>
+            <AccordionTrigger>Is Rudraksh&apos;s AI capable of recognizing multiple diseases?</AccordionTrigger>
+            <AccordionContent>Absolutely. Our AI model is trained on a diverse dataset, enabling it to recognize and diagnose a variety of plant diseases, ensuring comprehensive support for your plants&apos; well-being. </AccordionContent>
           </AccordionItem>
           <AccordionItem value="question-8">
             <AccordionTrigger>What if I have a unique or uncommon plant? Can Rudraksh still help?</AccordionTrigger>
-            <AccordionContent>Rudraksh's AI has been trained on a broad range of plants, and its capabilities extend to many species. While it might encounter limitations in extremely rare cases, our team is continuously working to improve coverage. </AccordionContent>
+            <AccordionContent>Rudraksh&apos;s AI has been trained on a broad range of plants, and its capabilities extend to many species. While it might encounter limitations in extremely rare cases, our team is continuously working to improve coverage. </AccordionContent>
           </AccordionItem>
           <AccordionItem value="question-9">
-            <AccordionTrigger>How do I access Rudraksh's features?</AccordionTrigger>
-            <AccordionContent>Simply access our user-friendly platform, upload a clear image of your plant's leaf, and Rudraksh's AI will do the rest, providing disease detection and personalized recommendations. </AccordionContent>
+            <AccordionTrigger>How do I access Rudraksh&apos;s features?</AccordionTrigger>
+            <AccordionContent>Simply access our user-friendly platform, upload a clear image of your plant&apos;s leaf, and Rudraksh&apos;s AI will do the rest, providing disease detection and personalized recommendations. </AccordionContent>
           </AccordionItem>
           <AccordionItem value="question-10">
             <AccordionTrigger>How can I provide feedback or report an issue with Rudraksh?</AccordionTrigger>
-            <AccordionContent>We value your input! Feel free to reach out through our provided contact channels or feedback forms. Your feedback helps us refine and enhance Rudraksh's capabilities.</AccordionContent>
+            <AccordionContent>We value your input! Feel free to reach out through our provided contact channels or feedback forms. Your feedback helps us refine and enhance Rudraksh&apos;s capabilities.</AccordionContent>
           </AccordionItem>
         </Accordion>
       </div>
