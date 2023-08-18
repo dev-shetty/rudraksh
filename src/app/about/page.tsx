4b2cc3db03fb9fb6788d58c1e94538f060e10126
Deveesh Shetty <deveeshshetty@gmail.com>
2023-08-18 14:31:51 +0530

    increases width of mdx paragraph

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ import AboutDocs from "@/content/docs/about.mdx"
 export default function About() {
   return (
     <main className="min-h-screen py-24 px-8">
-      <div className="prose prose-cyan dark:prose-dark">
+      <div className="mx-auto prose max-w-[80ch] prose-cyan dark:prose-dark">
         <AboutDocs />
       </div>
     </main>
