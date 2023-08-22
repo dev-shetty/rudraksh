b43edb95645a4d158cf79f6c896c14d916aa7ddd
Deveesh Shetty <deveeshshetty@gmail.com>
2023-08-22 16:42:51 +0530

    another import change

--- api/src/main.py
+++ api/src/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from torch.functional import split
-from src.ml_predict import predict_plant, Network
+from ml_predict import predict_plant, Network
 from pyfcm import FCMNotification
 import base64
 from decouple import config
